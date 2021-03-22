191588ea74eca0205f72959cf9097d3b171d0486
Puentebravo <puentematos@gmail.com>
2021-03-22 00:15:02 -0400

    fixed syntax in service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,11 @@
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open("static").then((cache) => {
-      return cache.addAll(["/", "./index.html", "./styles.css", "./index.js"]);
+      return cache.addAll(["/", "/index.html", "/styles.css", "/index.js"]);
     })
   );
   console.log("Install");
+  self.skipWaiting()
 });
 
 self.addEventListener('fetch', event => {
@@ -14,4 +15,4 @@ self.addEventListener('fetch', event => {
     })
   );
 });
-// To do: make app installable / Write code to allow storage of data in index DB/passing to database.
+// / Write code to allow storage of data in index DB/passing to database.
